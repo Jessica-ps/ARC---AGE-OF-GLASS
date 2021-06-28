b9ff727510846239a230e1d209f789d71f5b0a37
Jessica-ps <jessica.ps23@aluno.ifsc.edu.br>
2021-06-28 10:55:14 +0000

    commit

--- plugins/dados/dados.py
+++ plugins/dados/dados.py
@@ -1,18 +1,33 @@
-from errbot import BotPlugin, botcmd
+from  errbot  import  BotPlugin , botcmd
+de  importação aleatória  randint 
 
 
-class Example(BotPlugin):
-    """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
-    """
+classe  Dados ( BotPlugin ):
+    "" "
+    Rolar os dados para suporte a jogos de RPG de mesa.
+    Tem o rolar de n dados com m faces, entre outros comandos.
+    "" "
 
-    @botcmd  # flags a command
-    def tryme(self, msg, args):  # a command callable with !tryme
-        """
-        Execute to check if Errbot responds to command.
-        Feel free to tweak me to experiment with Errbot.
-        You can find me in your init directory in the subdirectory plugins.
-        """
-        return "It *works* !"  # This string format is markdown.
+    @ botcmd ( split_args_with = '' )
+    def  rolar ( self , msg , args ):
+        "" "
+        Informar quantos dados e faces.
+        Forma de usar:
+        ! rolar <dados> <faces>
+        onde <dados> e <faces> são números inteiros.
+        "" "
+
+        tente :
+            dados  =  int ( args [ 0 ])
+            faces  =  int ( args [ 1 ])
+        exceto :
+            yield  "Por favor, informe os dados e faces como números inteiros"
+            rendimento  "na seguinte forma:"
+            rendimento  "! rolar <dados> <faces>"
+        mais :
+            somatorio  =  0
+            para  parada  no  intervalo ( 1 , dados  +  1 ):
+                dado  =  randint ( 1 , faces )
+                somatorio  =  somatorio  +  dado
+                rendimento  "Dado"  +  str ( parada ) +  "="  +  str ( dado )
+            rendimento  "Somatório ="  +  str ( somatorio )
