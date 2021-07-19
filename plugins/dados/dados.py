ee93447b930f11cc47bfc59b475ab4a42268ffc5
Jessica-ps <jessica.ps23@aluno.ifsc.edu.br>
2021-07-19 10:40:42 +0000

    commit

--- plugins/dados/dados.py
+++ plugins/dados/dados.py
@@ -1,33 +1,33 @@
-from  errbot  import  BotPlugin , botcmd
-de  importação aleatória  randint 
+from errbot import BotPlugin, botcmd
+from random import randint
 
 
-classe  Dados ( BotPlugin ):
-    "" "
+class Dados(BotPlugin):
+    """
     Rolar os dados para suporte a jogos de RPG de mesa.
     Tem o rolar de n dados com m faces, entre outros comandos.
-    "" "
+    """
 
-    @ botcmd ( split_args_with = '' )
-    def  rolar ( self , msg , args ):
-        "" "
+    @botcmd(split_args_with=' ')
+    def rolar(self, msg, args):
+        """
         Informar quantos dados e faces.
         Forma de usar:
-        ! rolar <dados> <faces>
+        !rolar <dados> <faces>
         onde <dados> e <faces> são números inteiros.
-        "" "
+        """
 
-        tente :
-            dados  =  int ( args [ 0 ])
-            faces  =  int ( args [ 1 ])
-        exceto :
-            yield  "Por favor, informe os dados e faces como números inteiros"
-            rendimento  "na seguinte forma:"
-            rendimento  "! rolar <dados> <faces>"
-        mais :
-            somatorio  =  0
-            para  parada  no  intervalo ( 1 , dados  +  1 ):
-                dado  =  randint ( 1 , faces )
-                somatorio  =  somatorio  +  dado
-                rendimento  "Dado"  +  str ( parada ) +  "="  +  str ( dado )
-            rendimento  "Somatório ="  +  str ( somatorio )
+        try:
+            dados = int(args[0])
+            faces = int(args[1])
+        except:
+            yield "Por favor, informe dados e faces como números inteiros"
+            yield "na seguinte forma:"
+            yield "!rolar <dados> <faces>"
+        else:
+            somatorio = 0
+            for parada in range(1, dados + 1):
+                dado = randint(1, faces)
+                somatorio = somatorio + dado
+                yield "Dado " + str(parada) + " = " + str(dado)
+            yield "Somatório = " + str(somatorio)
