e3a4baca062e57fc4213c9b5d537361ff7b7242b
Jessica-ps <jessica.ps23@aluno.ifsc.edu.br>
2021-07-26 15:44:09 +0000

    integrar Errbot e Discord 8

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
-import logging  
+import logging
 from os import environ
+from dotevn import load_dotenv
+load_dotenv
 
 BACKEND = 'Discord'  
-BOT_IDENTITY = {
-'token' : environ.get('DISCORD_TOKEN')
+BOT_IDENTITY = {'token' : environ.get('DISCORD_TOKEN')
 }
-BOT_ADMINS = ['Jéssica#7541', ]
+BOT_ADMINS = (environ.get('BOT_ADMIN'), )  
+BOT_PREFIX = '!'
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
 
 BOT_DATA_DIR = r'data'
 BOT_EXTRA_PLUGIN_DIR = r'plugins'
-BOT_EXTRA_BACKEND_DIR = r'backend'
+BOT_EXTRA_PLUGIN_DIR = r'Backend'
 
 BOT_LOG_FILE = r'errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
-
-BOT_ADMINS = ('@CHANGE_ME', ) 
