5aaf975872d8e21ffa1cbd88a4d393dcdf065262
Jessica-ps <jessica.ps23@aluno.ifsc.edu.br>
2021-07-26 16:17:37 +0000

    commit

--- config.py
+++ config.py
@@ -4,15 +4,19 @@ from dotevn import load_dotenvn
 load_dotenv
 
 BACKEND = 'Discord'  
+
 BOT_IDENTITY = {'token' : environ.get('DISCORD_TOKEN')
 }
 BOT_ADMINS = (environ.get('BOT_ADMIN'), )  
+
 BOT_PREFIX = '!'
 BOT_PREFIX_OPTIONAL_ON_CHAT = True
 
 BOT_DATA_DIR = r'data'
 BOT_EXTRA_PLUGIN_DIR = r'plugins'
 BOT_EXTRA_PLUGIN_DIR = r'Backend'
+
+AUTOINSTALL_DEPS = True
 
 BOT_LOG_FILE = r'errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
