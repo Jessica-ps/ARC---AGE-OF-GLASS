b13ee764a6421d6ffc885e2314f50655e619507f
Jessica-ps <jessica.ps23@aluno.ifsc.edu.br>
2021-08-05 12:46:48 +0000

    commit

--- plugins/labirinto/labirinto.py
+++ plugins/labirinto/labirinto.py
@@ -1,15 +1,16 @@
-fron errbot import BotPlugin, bakand
+from errbot import BotPlugin, botcmd
 
 
 class Labirinto(BotPlugin):
     """
     Jogo de labirinto feito em matriz de números inteiros.
     O objetivo é fazer com que o jogador saia do labirinto,
-    e para isso ele tem que andar por corredores.
+    e para isso será preciso movimentar-se pelas salas e
+    corredores.
     Internamente, o jogo implementa uma matriz de inteiros
     para armazenar informações como:
-    - Parede: 1
-    - Corredor: 0
+    - Parede: 0
+    - Sala ou corredor: 1
     - Posição e sentido do jogador:
       -  2: sentido Norte
       -  4: sentido Sul
@@ -17,10 +18,10 @@ class Labirinto(BotPlugin):
       - 16: sentido Leste
     Assim, o mapa acumula informações com base nessas
     potências de dois, como por exemplo:
-    5 = 4 + 1 = jogador no sentido Sul + sala ou corredor.
+    5 = 4 + 1 = jogador no sentido Sul + sala ou corredor    
     """
 
-    mapa_inteiros = [[1, 1, 1, 8, 1],
+    mapa_inteiros = [[1, 1, 1, 9, 1],
                      [1, 0, 0, 0, 1],
                      [1, 0, 0, 1, 1],
                      [1, 1, 0, 0, 1],
@@ -30,7 +31,7 @@ class Labirinto(BotPlugin):
     def mapa(self, msg, args):
         """
         Apresentar o mapa do bot.
-        
         """
-        for linhas in self.mapa_inteiros:
+        
+        for linha in self.mapa_inteiros:
             yield " ".join(map(str, linha))
